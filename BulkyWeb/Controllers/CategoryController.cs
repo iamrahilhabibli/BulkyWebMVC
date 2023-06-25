13eafcb6e28b94939fe3b1b3f955dff385cd5c82
Rahil Habibli <rahilah@code.edu.az>
2023-06-26 00:08:59 +0400

    Server side validations added to form

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -6,25 +6,29 @@ namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
-    private readonly AppDbContext _db;
-    public CategoryController(AppDbContext db)
-    {
-        _db = db;
-    }
-    public IActionResult Index()
+	private readonly AppDbContext _db;
+	public CategoryController(AppDbContext db)
 	{
-        List<Category> objCategoryList = _db.Categories.ToList();
+		_db = db;
+	}
+	public IActionResult Index()
+	{
+		List<Category> objCategoryList = _db.Categories.ToList();
 		return View(objCategoryList);
 	}
-    public IActionResult Create()
-    {
-        return View();
-    }
-    [HttpPost]
+	public IActionResult Create()
+	{
+		return View();
+	}
+	[HttpPost]
 	public IActionResult Create(Category obj)
 	{
-        _db.Categories.Add(obj);
-        _db.SaveChanges();
-		return RedirectToAction(nameof(Index));
+		if (ModelState.IsValid)
+		{
+			_db.Categories.Add(obj);
+			_db.SaveChanges();
+			return RedirectToAction(nameof(Index));
+		}
+		return View();
 	}
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -9,7 +9,9 @@ public class Category
     public int Id { get; set; }
     [Required]
     [DisplayName("Category Name")]
+    [MaxLength(30)]
     public string Name { get; set; }
     [DisplayName("Display Order")]
+    [Range(1,100)]
     public int DisplayOrder { get; set; }
 }
