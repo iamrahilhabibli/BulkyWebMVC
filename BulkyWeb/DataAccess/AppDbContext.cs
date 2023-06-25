f8dfc716bda6f7293b9f5aebd1311fe6b97d3946
Rahil Habibli <rahilah@code.edu.az>
2023-06-25 22:45:03 +0400

    DbTable Added

--- BulkyWeb/DataAccess/AppDbContext.cs
+++ BulkyWeb/DataAccess/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.DataAccess;
 
@@ -6,4 +7,5 @@ public class AppDbContext:DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+    public DbSet<Category> Categories { get; set; }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -4,7 +4,7 @@ namespace BulkyWeb.Models;
 
 public class Category
 {
-    [Key] // <= This is DataAnnotation
+    [Key] // <= This is DataAnnotation - You can use this if PK name would be anything custom
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
